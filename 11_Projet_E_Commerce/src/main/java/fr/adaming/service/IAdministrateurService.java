8f1c58194b95e208531f7a4e9d2f152da9f7b32e
Adaming44 <Adaming44@192.168.1.145>
2018-01-17 17:04:08 +0100

    cc

--- 11_Projet_E_Commerce/src/main/java/fr/adaming/service/IAdministrateurService.java
+++
@@ -1,9 +0,0 @@
-package fr.adaming.service;
-
-
-import fr.adaming.model.Administrateur;
-
-public interface IAdministrateurService {
-
-	public Administrateur isExist(Administrateur a) throws Exception;
-}
